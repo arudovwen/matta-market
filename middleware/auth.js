beb02f4f3c875046ec22985ee9101b602f21a07c
Success Ahon <40746773+arudovwen@users.noreply.github.com>
2024-02-06 10:37:29 +0100

    Update

--- middleware/auth.js
+++ middleware/auth.js
@@ -13,7 +13,7 @@ export default defineNuxtRouteMiddleware((to, from) => {
     return navigateTo(`/auth/login?redirected_from=${to.path}`);
   }
 
-  if (authStore.userType.toLowerCase() === "buyer" && !buyerRoutes.includes(to.name)) {
+  if (authStore?.isLoggedIn && authStore?.userType?.toLowerCase() === "buyer" && !buyerRoutes.includes(to.name)) {
     return navigateTo("/");
   }
 });
