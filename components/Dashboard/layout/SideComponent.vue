e86a5b0bcbf5300a424fe102b9fedef27b95e16d
Success Ahon <40746773+arudovwen@users.noreply.github.com>
2024-02-26 00:57:04 +0100

    Update SideComponent.vue

--- components/Dashboard/layout/SideComponent.vue
+++ components/Dashboard/layout/SideComponent.vue
@@ -1,15 +1,7 @@
 <template>
   <aside
-    class="z-[9999] bg-white h-screen relative py-5 border-r border-[#EAECF0] shadow-[0px_2px_4px_0px_rgba(0,0,0,0.04)] min-w-[245px]"
+    class="z-[9] bg-white h-screen relative py-5 border-r border-[#EAECF0] shadow-[0px_2px_4px_0px_rgba(0,0,0,0.04)] min-w-[245px]"
   >
-    <div class="logo mb-7 px-5">
-      <NuxtLink to="/">
-        <img
-          src="/images/logo.png"
-          alt="Matta"
-          class="w-20 md:w-[100px] h-auto object-contain"
-      /></NuxtLink>
-    </div>
     <nav class="h-full">
       <ul class="grid grid-cols-1 gap-y-1">
         <li v-for="item in mappedNav" :key="item.name">
@@ -18,9 +10,7 @@
             :to="item.url"
             class="text-sm flex items-center px-5 border-r-[3px] border-transparent font-medium hover:bg-[#2270FA0F] hover:text-primary-500 hover:border-primary-500"
             :activeClass="`${
-              storeOpen
-                ? ''
-                : ' bg-[#2270FA0F] text-primary-500 block'
+              storeOpen ? '' : ' bg-[#2270FA0F] text-primary-500 block'
             }`"
           >
             <span class="flex items-center gap-x-[10px] flex-1 py-[9px]">
@@ -32,8 +22,11 @@
             v-else
             @click="storeOpen = true"
             class="text-sm flex items-center px-5 border-r-[3px] border-transparent group font-medium hover:bg-[#2270FA0F] hover:text-primary-500 hover:border-primary-500 cursor-pointer"
-            :class="` ${storeOpen ||  route.path.includes('storefront') ? 'bg-[#2270FA0F] text-primary-500' : ''}`"
-         
+            :class="` ${
+              storeOpen || route.path.includes('storefront')
+                ? 'bg-[#2270FA0F] text-primary-500'
+                : ''
+            }`"
           >
             <span class="flex items-center gap-x-[10px] flex-1 py-[9px]">
               <AppIcon :icon="item.icon" iconClass="text-xl text-[#667085]" />
