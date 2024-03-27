345630fb167e8ee93b82b851bb915cf912988454
Success Ahon <40746773+arudovwen@users.noreply.github.com>
2024-03-27 14:19:36 +0100

    Update Documents.vue

--- components/onboarding/company/Documents.vue
+++ components/onboarding/company/Documents.vue
@@ -11,7 +11,7 @@
         </p>
       </div>
       <div
-        v-if="!companyInfo.companyDocuments || companyInfo.companyDocuments.find((i) => !i.url)"
+        v-if="!companyInfo.companyDocuments || companyInfo.companyDocuments?.some((i) => !i.url)"
         class="grid gap-y-6 max-w-[560px] w-full"
       >
         <div>
