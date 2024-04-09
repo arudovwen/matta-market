6097a616544a959d8b73ea59893ef97aa33693f6
Success Ahon <40746773+arudovwen@users.noreply.github.com>
2024-04-09 08:58:36 +0100

    Update Detail.vue

--- components/product/Detail.vue
+++ components/product/Detail.vue
@@ -89,30 +89,11 @@
               text="Request sample"
               btnClass="!rounded-[5px] !text-[#333] px-[15px] !py-[6px] text-xs sm:text-sm border border-[#DBDBDB]"
             />
-            <!-- <AppButton
-              @click="handleOrderRequest()"
-              text="Add to order request"
+            <AppButton
+              @click="handleRequest('quote')"
+              text="Request quote"
               btnClass="!rounded-[5px] !text-[#333] px-[15px] !py-[6px] text-xs sm:text-sm border border-[#DBDBDB] "
-            /> -->
-            <!-- <Tooltip title="tooltip test" description="Hello">
-              <span class="cursor-pointer">
-                <AppIcon
-                  icon="bi:question-circle"
-                  iconClass="text-[#98A2B3]" /></span
-            ></Tooltip> -->
-            <!-- <tippy
-              tag="button"
-              content-tag="div"
-              content-class="content-wrapper"
-            >
-              <template #default>
-                <span class="cursor-pointer">
-                  <AppIcon
-                    icon="bi:question-circle"
-                    iconClass="text-[#98A2B3]" /></span
-              ></template>
-              <template #content>Hi!</template>
-            </tippy> -->
+            />
           </div>
           <!-- <AppButton
             @click="handleSave"
@@ -381,9 +362,9 @@ const mypackage = computed(() =>
   selectedPackage.value ? JSON.parse(selectedPackage.value) : null
 );
 const counter = ref(1);
-const cartLoading = ref(false)
+const cartLoading = ref(false);
 function handleCart(type) {
-  cartLoading.value = true
+  cartLoading.value = true;
   if (!selectedPackage.value) {
     toast.info("Please choose a package");
     return;
@@ -409,7 +390,7 @@ function handleCart(type) {
 
   cartStore?.addToCart(data, type).then((res) => {
     if (!res.status && res.message === "incart") {
-     toast.info("Already in cart")
+      toast.info("Already in cart");
     }
     if (res.status && res.message !== "buy") {
       isAdded.value = true;
@@ -420,7 +401,7 @@ function handleCart(type) {
     if (res.message === "buy") {
       router.push("/cart");
     }
-    cartLoading.value = false
+    cartLoading.value = false;
   });
 }
 function handleSave() {
