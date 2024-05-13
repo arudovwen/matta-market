fab2935b035319333f265b6ec9ec1767ceb54136
Success Ahon <40746773+arudovwen@users.noreply.github.com>
2024-05-13 10:52:13 +0100

    Update Top.vue

--- components/BuyChemical/Top.vue
+++ components/BuyChemical/Top.vue
@@ -5,7 +5,7 @@
       data-aos-once="true"
       class="text-4xl md:text-6xl font-semibold mb-6"
     >
-      Source your &#123;<span class="text-primary-500">{{`${words[0]}`}}</span>&#125; <br /> all in one place
+      <span class="text-primary-500">{{`${words[0]}`}}</span> <br /> all in one place
     </h1>
     <p
       data-aos="fade-in"
