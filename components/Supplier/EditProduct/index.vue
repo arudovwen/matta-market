40a40d5005b16c86939b3991ea4bc9495b975779
Tolu <tolulopeoduro345@gmail.com>
2024-07-05 16:08:08 +0100

    update

--- components/Supplier/EditProduct/index.vue
+++ components/Supplier/EditProduct/index.vue
@@ -46,6 +46,7 @@ import {
   getFeaturedManufacturer,
 } from "~/services/productservices";
 import Preview from "~/components/preview/IndexPreview";
+import { defaultPropertyItems } from "~/utils/constants";
 
 const route = useRoute();
 const router = useRouter();
@@ -73,27 +74,42 @@ const tabs = [
   },
 ];
 
-const links = [
+const selectedMeasurement = ref(measurements[0]);
+const defaultPackagesAvailable = [
   {
-    title: "home",
-    url: "/",
-  },
-  {
-    title: "storefront",
-    url: "/overview",
-  },
-  {
-    title: "products",
-    url: "/storefront/products",
-  },
-  {
-    title: "edit product",
-    url: "/#",
+    package: {
+      id: create_UUID(),
+      title: "",
+    },
+    unit: selectedMeasurement.value ? selectedMeasurement.value.value : "",
+    size: null,
+    amount: null,
+    isAvailable: false,
+    color: "",
+    purity: "",
   },
 ];
+
+// const links = [
+//   {
+//     title: "home",
+//     url: "/",
+//   },
+//   {
+//     title: "storefront",
+//     url: "/overview",
+//   },
+//   {
+//     title: "products",
+//     url: "/storefront/products",
+//   },
+//   {
+//     title: "edit product",
+//     url: "/#",
+//   },
+// ];
 const active = ref(1);
 
-const selectedMeasurement = ref(measurements[0]);
 const product = ref({
   id: "",
   ProductId: "",
@@ -169,28 +185,6 @@ const form = reactive({
     },
   ],
 });
-const defaultPropertyItems = {
-  features: {
-    propertyItems: [],
-    subSection: [],
-  },
-  applications: {
-    propertyItems: [{ property: null, propertyValue: [] }],
-    subSection: [],
-  },
-  property: {
-    propertyItems: [{ property: null, propertyValue: [] }],
-    subSection: [],
-  },
-  compliance: {
-    propertyItems: [],
-    subSection: [],
-  },
-  technical: {
-    propertyItems: [{ property: null, propertyValue: [] }],
-    subSection: [],
-  },
-};
 
 function togglePreview() {
   isPreviewing.value = !isPreviewing.value;
@@ -229,85 +223,12 @@ onBeforeMount(() => {
     isLoading.value = true;
     getSupplierProduct(queryParams).then((res) => {
       product.value = res.data.data;
-      updateData();
+      updateData(form, product, defaultPackagesAvailable, isLoading, route);
     });
   }
   getProducers();
 });
-function updateData() {
-  form.id = route.query.id;
-  form.ProductId = route.query.id;
-  form.pickUpLocationId = product.value.pickupLocationId;
-  form.name = product.value.name;
-  form.unit = product.value.packagesAvailable
-    ? product.value.packagesAvailable[0].unit
-    : "";
-  form.manufacturer = product.value.manufacturer || [];
-  form.markets = product.value.markets || [];
-  form.marketApplications = product.value.marketApplications || [];
-  form.marketSubapplications = product.value.marketSubapplications || [];
-  form.technologies = product.value.technologies || [];
-  form.techApplications = product.value.techApplications || [];
-  form.techSubApplications = product.value.techSubApplications || [];
-  form.description = product.value.description;
-  form.gallery = product.value.gallery || [];
-  form.price = product.value.price;
-  form.sampleAvailable = product.value.sampleAvailable;
-  form.packagesAvailable = product.value.packagesAvailable || [
-    {
-      package: {
-        id: create_UUID(),
-        title: "",
-      },
-      unit: selectedMeasurement.value ? selectedMeasurement.value.value : "",
-      size: null,
-      amount: null,
-      isAvailable: false,
-      color: "",
-      purity: "",
-    },
-  ];
-  form.packages = product.value.packages || [];
-  form.hideProduct = product.value.hideProduct;
-  form.hidePrice = product.value.hidePrice;
-  form.productBrandName = product.value.productBrandName;
-  form.documents = product.value.documentInfoModels || [];
-  form.productExperts = product.value.productExperts || [];
-  form.productQuestions = product.value.productQuestions || [];
-  form.tags = product.value.tags || [];
-  form.supplierId = product.value.supplierId;
-  form.propertyItems =
-    product.value.propertyItems?.propertyItems &&
-    Object.keys(product.value.propertyItems.propertyItems).length
-      ? product.value.propertyItems.propertyItems
-      : defaultPropertyItems;
-  form.documentproperties = [
-    {
-      text: "Material safety data sheet (MSDS)",
-      item: "Info such as the chemical properties.",
-      value: "Material safety data sheet (MSDS)",
-    },
-    {
-      text: "Certificate of analysis (COA)",
-      item: "Certificate of analysis of product.",
-      value: "Certificate of analysis (COA)",
-    },
-    {
-      text: "Technical data sheet (TDS)",
-      item: "Document with technical data of product.",
-      value: "Technical data sheet (TDS)",
-    },
-    {
-      text: "Other",
-      item: "Other types of product documents.",
-      value: "other",
-    },
-  ];
-  form.properties = product.value?.propertyItems?.properties || [];
-  form.propertyValueList =
-    product.value?.propertyItems?.propertyValueList || [];
-  isLoading.value = false;
-}
+
 function getProducers() {
   getFeaturedManufacturer({
     Search: "",
@@ -324,7 +245,7 @@ watch(route, () => {
     isLoading.value = true;
     getSupplierProduct(queryParams).then((res) => {
       product.value = res.data.data;
-      updateData();
+      updateData(form, product, defaultPackagesAvailable, isLoading, route);
     });
   }
 });
