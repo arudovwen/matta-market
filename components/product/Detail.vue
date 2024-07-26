5a454b6bbba9642aab71b1bc963b98bcfadbb30b
Success Ahon <40746773+arudovwen@users.noreply.github.com>
2024-07-26 10:59:42 +0100

    Update

--- components/product/Detail.vue
+++ components/product/Detail.vue
@@ -473,7 +473,6 @@ watch(
   }
 );
 watch(productData, () => {
-  console.log(productData);
   supplierStore.fetchSupplier(productData.value.supplierId);
   imageUrl.value = productData?.value?.featuredPhoto;
 });
