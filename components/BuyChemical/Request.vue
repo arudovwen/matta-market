6eaa3b2960d8bc5e98f9b1d222c757ded8c7271f
Success Ahon <40746773+arudovwen@users.noreply.github.com>
2024-08-09 11:59:41 +0100

    update

--- components/BuyChemical/Request.vue
+++ components/BuyChemical/Request.vue
@@ -2,7 +2,7 @@
     <section class=" py-[96px]">
       <div class="container grid grid-cols-1 md:grid-cols-2 gap-y-8 md:gap-x-16 md:gap-y-0">
        
-        <div class="w-full  h-[200px] lg:h-[500px] bg-cover bg-[url('/images/request.png')] rounded-lg"></div>
+        <div class="w-full  h-[200px] lg:h-[500px] bg-cover bg-[url('/buyleft.png')] rounded-lg"></div>
         <div class="flex-1 flex items-center  flex-col md:flex-row">
       <div class="max-w-[630px]">
         <h2 class="font-semibold text-xl md:text-[36px] leading-[1.2] mb-4 md:mb-8">
