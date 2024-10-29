9eeeaeb939a172963b889c985f871f9e6d604d29
Success Ahon <successahon@gmail.com>
2024-10-29 23:35:18 +0100

    Update

--- components/Checkout/Discount.vue
+++ components/Checkout/Discount.vue
@@ -1,8 +1,5 @@
 <template>
   <div class="bg-white rounded-b-[10px] border-t border-[#f3f3f3]">
-    <!-- <h2 class="px-[30px] py-5 font-bold text-xl border-b border-[#f3f3f3]">
-      Promotion
-    </h2> -->
     <form
       @submit.prevent="handleSubmit"
       class="px-[30px] pt-6 pb-[30px] flex items-center gap-x-3"
@@ -24,7 +21,9 @@
     </form>
   </div>
 </template>
+
 <script setup>
+import { ref, watch } from "vue";
 import { toast } from "vue3-toastify";
 import { applyDiscount, getDiscountByCode } from "~/services/cartservice";
 
@@ -33,7 +32,7 @@ const loading = ref(false);
 const cartStore = useCartStore();
 const isApplied = ref(false);
 const firstTimeCode = "1ST50KOFF";
-onMounted(() => {});
+
 function handleSubmit() {
   loading.value = true;
   applyDiscount({
@@ -42,47 +41,45 @@ function handleSubmit() {
   })
     .then((res) => {
       if (res.status === 200) {
-        cartStore.getMyCart();
-        loading.value = false;
         isApplied.value = true;
         toast.success("Discount applied");
+        cartStore.getMyCart();
       }
     })
     .catch((err) => {
-      // toast.error(
-      //   err?.response?.data?.message ||
-      //     err?.response?.data?.Message ||
-      //     "Invalid code"
-      // );
+      const errorMsg =
+        err?.response?.data?.message ||
+        err?.response?.data?.Message ||
+        "Invalid code";
+      toast.error(errorMsg);
+    })
+    .finally(() => {
       loading.value = false;
     });
 }
+
 function handleFirst() {
-  if (
-    cartStore?.cartData?.firstOrder &&
-    cartStore.cartId &&
-    isApplied.value == false
-  ) {
-    getDiscountByCode(firstTimeCode).then((res) => {
-      if (
-        res.status === 200 &&
-        cartStore?.cartTotalAmount > res.data.minimumOrderValue
-      ) {
-        code.value = firstTimeCode;
-        handleSubmit();
-        return;
-      }
-    });
-  }
-  if (!cartStore?.cartData?.firstOrder && cartStore.cartId && code.value) {
-    handleSubmit();
-    return;
+  if (cartStore?.cartData?.firstOrder && cartStore.cartId && !isApplied.value) {
+    getDiscountByCode(firstTimeCode)
+      .then((res) => {
+        if (
+          res.status === 200 &&
+          cartStore?.cartTotalAmount > res.data.minimumOrderValue
+        ) {
+          code.value = firstTimeCode;
+          handleSubmit();
+        }
+      })
+      .catch(() => {
+        toast.error("Error applying first-time discount");
+      });
   }
 }
+
 watch(
-  () => [cartStore?.cartData],
+  () => cartStore?.cartData,
   () => {
-    handleFirst();
+    if (!isApplied.value) handleFirst();
   }
 );
 </script>
