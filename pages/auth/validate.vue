2fbb6c4ddd30e5c181f1efa5bb52fee1c7b9a2d0
Success Ahon <successahon@gmail.com>
2025-02-17 18:57:33 +0100

    Update validate.vue

--- pages/auth/validate.vue
+++ pages/auth/validate.vue
@@ -2,7 +2,7 @@
   <div class="flex justify-center items-center p-10 h-screen w-screen">
     <div class="text-center flex flex-col justify-center items-center gap-y-6">
       <AppLoaderV2 />
-      <span class="text-sm text-center block">Validating user</span>
+      <span class="text-sm text-center block">Signing you in</span>
     </div>
   </div>
 </template>
