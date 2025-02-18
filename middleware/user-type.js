baa8c977e8b5ab12ef520e906f7338d555d0c1dc
Success Ahon <successahon@gmail.com>
2025-02-18 20:14:06 +0100

    Update

--- middleware/user-type.js
+++ middleware/user-type.js
@@ -7,19 +7,19 @@ export default defineNuxtRouteMiddleware(async (to) => {
   const isLoggedIn = !!mattaAuth.value;
   const businessUserType = mattaAuth.value?.businessUserType;
 
-  if (isLoggedIn) {
-    // Redirect if the user doesn't have businessUserType and is not already on the user-type route
-    if (!businessUserType && to.name !== "user-type") {
-      return navigateTo("/user-type");
-    }
+//   if (isLoggedIn) {
+   
+//     if (!businessUserType && to.name !== "user-type") {
+//       return navigateTo("/user-type");
+//     }
 
-    // Redirect if user is already on the user-type page or if logged in with businessUserType
-    if (to.name === "user-type" && businessUserType) {
-      return navigateTo("/");
-    }
-  }
-  if (!isLoggedIn && to.name === "user-type") {
-    return navigateTo("/");
-  }
+//     // Redirect if user is already on the user-type page or if logged in with businessUserType
+//     if (to.name === "user-type" && businessUserType) {
+//       return navigateTo("/");
+//     }
+//   }
+//   if (!isLoggedIn && to.name === "user-type") {
+//     return navigateTo("/");
+//   }
   return;
 });
