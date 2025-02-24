0419a826ca6baa9b89727506e19bf98145c6d831
tolulopeoduro <tolulopeodduro345@gmail.com>
2025-02-24 09:14:00 +0100

    fix: remove disabled state from company email input field

--- components/Supplier/FinanceRequest/Trade/Kyb.vue
+++ components/Supplier/FinanceRequest/Trade/Kyb.vue
@@ -35,7 +35,6 @@
         v-bind="emailAtt"
         v-model="companyEmail"
         :error="errors.companyEmail"
-        :disabled="!!companyEmail"
         :isCumpulsory="true"
       />
       <FormGroup
