e8f56615f395ca52012766f4d4664fb20e7e5d7e
Success Ahon <successahon@gmail.com>
2025-02-24 23:53:17 +0100

    update

--- components/VerificationBox.vue
+++ components/VerificationBox.vue
@@ -1,6 +1,7 @@
 <template>
+  {{ authStore?.userInfo?.onboardingPageStatus }}
   <div
-    v-if="!authStore?.userInfo?.onboardingPageStatus"
+    v-if="!authStore?.userInfo?.onboardingStatus"
     class="px-5 py-[14px] bg-[#182230] rounded-[5px] flex flex-col lg:flex-row justify-between gap-y-5 lg:gap-y-0 lg:gap-x-40 relative mb-3"
   >
     <div class="flex gap-x-4 items-center">
