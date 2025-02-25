1d7b9c7e92f46bbcf1b25f9ffd638ef54dc6d251
Success Ahon <successahon@gmail.com>
2025-02-25 12:15:34 +0100

    update

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -206,7 +206,7 @@
                           <button
                             v-else-if="n.key === 'settings'"
                             @click="
-                              navigateTo('https://dev.profile.matta.trade', {
+                              navigateTo('https://staging.profile.matta.trade', {
                                 open: {
                                   target: '_blank',
                                 },
