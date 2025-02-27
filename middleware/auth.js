3c0b5241705d7499e6fcf54f98a5646a974206d9
Success Ahon <successahon@gmail.com>
2025-02-27 15:11:36 +0100

    Update

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,21 +4,15 @@ export default defineNuxtRouteMiddleware(async (to) => {
   const mattaAuth = useCookie("mattaAuth", defaultOptions);
   const isLoggedIn = !!mattaAuth.value;
 
-  // Handle not logged in
   if (!isLoggedIn) {
-    // Redirect using the handleRouting function to open a new window or continue in the same window.
     handleRouting({ target: "_self" });
-    return; // Stop further processing
+    return;
   }
 
-  // Handle businessUserType check if logged in
   if (isLoggedIn) {
     const businessUserType = mattaAuth.value.businessUserType;
-  
-    // Check if businessUserType is not 0 or 1
     if (businessUserType !== 0 && businessUserType !== 1 && to.path !== '/checkout') {
-      return navigateTo("/user-type"); // Redirect to /user-type page
+      return navigateTo("/user-type");
     }
   }
-  
 });
