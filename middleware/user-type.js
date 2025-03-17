be854187d2783981022e04d172cbdd41fa2044d9
Success Ahon <successahon@gmail.com>
2025-03-17 18:35:35 +0100

    updating env

--- middleware/user-type.js
+++ middleware/user-type.js
@@ -1,7 +1,7 @@
 import { handleRouting } from "~/utils/constants";
 
 export default defineNuxtRouteMiddleware(async (to) => {
-  const mattaAuth = useCookie("mattaAuth_Dev", defaultOptions);
+  const mattaAuth = useCookie("mattaAuth_Staging", defaultOptions);
   const routeName = to?.name?.toString() || "";
   const isAuthRoute = routeName.includes("auth");
   const isLoggedIn = !!mattaAuth.value;
