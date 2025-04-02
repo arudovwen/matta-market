c2a705d8c02d3e0072242f74988d38eaf6f3cce2
Success Ahon <successahon@gmail.com>
2025-04-02 16:59:55 +0100

    Update cartservice.js

--- services/cartservice.js
+++ services/cartservice.js
@@ -24,25 +24,25 @@ export async function removecartitem(data) {
   return await marketPost(`${urls.REMOVE_CART}/${data}`, data, config);
 }
 export async function deleteAddress(data) {
-  return await post(`${urls.DELETE_SHIPPING}/${data}`, data, config);
+  return await marketPost(`${urls.DELETE_SHIPPING}/${data}`, data, config);
 }
 export async function addshipping(data) {
-  return await post(urls.ADD_SHIPPING_ADDRESS, cleanObject(data), config);
+  return await marketPost(urls.ADD_SHIPPING_ADDRESS, cleanObject(data), config);
 }
 
 export async function editshipping(data) {
-  return await post(urls.EDIT_SHIPPING_ADDRESS, data, config);
+  return await marketPost(urls.EDIT_SHIPPING_ADDRESS, data, config);
 }
 export async function applyDiscount(data) {
-  return await post(urls.APPLY_DISCOUNT, data, config);
+  return await marketPost(urls.APPLY_DISCOUNT, data, config);
 }
 
 export async function getDiscountByCode(data) {
-  return await get(urls.GET_DISCOUNT(data), config);
+  return await marketGet(urls.GET_DISCOUNT(data), config);
 }
 
 export async function setdefaultaddress(addressId) {
-  return await post(
+  return await marketPost(
     `${urls.DEFAULT_SHIPPING_ADDRESS}/${addressId}`,
     "",
     config
@@ -50,7 +50,7 @@ export async function setdefaultaddress(addressId) {
 }
 
 export const getalladdress = withRetryHandling(() => {
-  return get(`${urls.GET_SHIPPING_ADDRESS}`, config);
+  return marketGet(`${urls.GET_SHIPPING_ADDRESS}`, config);
 });
 
 export const getallpickuplocations = withRetryHandling(() => {
@@ -68,17 +68,17 @@ export async function editPickupLocation(data) {
   return await put(`${urls.EDIT_PICKUP_ADDRESS}/${data.id}`, data, config);
 }
 export async function confirmpurchase(data) {
-  return await post(`${urls.CONFIRM_PURCHASE}`, data, config);
+  return await marketPost(`${urls.CONFIRM_PURCHASE}`, data, config);
 }
 export async function requestACall(data) {
-  return await get(`${urls.REQUEST_A_CALL}`, data, config);
+  return await marketGet(`${urls.REQUEST_A_CALL}`, data, config);
 }
 
 export async function shippingBreakdown() {
-  return await get(`${urls.SHIPPONG_COST_BREAKDOWN}`, config);
+  return await marketGet(`${urls.SHIPPONG_COST_BREAKDOWN}`, config);
 }
 export async function confirmpayment(data) {
-  return await post(
+  return await marketPost(
     `${urls.CONFIRM_PAYMENT}`,
     data,
     config
@@ -86,13 +86,13 @@ export async function confirmpayment(data) {
 }
 
 export async function getcartorder(data) {
-  return await get(
+  return await marketGet(
     `${urls.GET_ORDER}?${new URLSearchParams(cleanObject(data))}`,
     config
   );
 }
 export async function getcartcustomer(data) {
-  return await get(
+  return await marketGet(
     `${urls.GET_CUSTOMER_INFO}?${new URLSearchParams(cleanObject(data))}`,
     config
   );
