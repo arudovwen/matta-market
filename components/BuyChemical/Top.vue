3114929626d217baa836a3c2d1dd5356fe0c6fcc
Success Ahon <40746773+arudovwen@users.noreply.github.com>
2025-05-17 11:44:34 +0100

    Updating Signup url

--- components/BuyChemical/Top.vue
+++ components/BuyChemical/Top.vue
@@ -21,7 +21,7 @@
 
     <div  class="flex gap-x-4 mb-[64px]">
       <AppButton
-        @click="navigateTo('/auth/register')"
+        @click="handleRouting('register')"
         text="Sign up for free"
         btnClass="bg-transparent border-[#D0D5DD] border !px-[18px] !py-3 text-[#344054]"
       />
