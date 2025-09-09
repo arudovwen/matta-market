6904fa2a7e2512d00db646f1d54537ebbfe65676
Success Ahon <40746773+arudovwen@users.noreply.github.com>
2025-09-09 15:12:13 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -3,17 +3,19 @@
     <LandingBanner />
 
     <div class="bg-[#F4F4F4] darks:bg-gray-700 pt-8 sm:pt-[60px]">
-      <div class="hidden lg:block mb-6 sm:mb-10 container">
+      <div class="container hidden mb-6 lg:block sm:mb-10">
         <LandingBoard
           link="https://matta.trade/product/ASPARTAME/3009649000015519001?categoryId="
+          url="https://res.cloudinary.com/arudovwen-me/image/upload/v1757331185/Signature2-ezgif.com-video-to-gif-converter_1_zlt4lc.gif"
         />
       </div>
       <div class="pb-8 xl:pb-[70px]">
         <LandingHistory />
         <LandingHot tag="hotdeals" />
-        <div class="lg:hidden mb-6 sm:mb-10 container">
+        <div class="container mb-6 lg:hidden sm:mb-10">
           <LandingBoard
             link="https://matta.trade/product/ASPARTAME/3009649000015519001?categoryId="
+            url="https://res.cloudinary.com/arudovwen-me/image/upload/v1757331185/Signature2-ezgif.com-video-to-gif-converter_1_zlt4lc.gif"
           />
         </div>
         <!-- <LandingRecommended tag="recommended" /> -->
@@ -28,7 +30,7 @@
       </div>
       <LandingInfo />
     </div>
-    <div class="bg-white pt-6 md:pt-10">
+    <div class="pt-6 bg-white md:pt-10">
       <LandingCategories />
       <LandingBrands />
       <LandingTestimonials />
