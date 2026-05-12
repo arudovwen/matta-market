533a43fa91483568ac553ec9c12da82ad41e8f45
Success Ahon <40746773+arudovwen@users.noreply.github.com>
2026-05-12 22:11:33 +0100

    Update deltaservice.js

--- services/deltaservice.js
+++ services/deltaservice.js
@@ -1,11 +1,6 @@
 import urls from "../helpers/url_helpers";
-import { createApiClient } from "~/helpers/api_helpers";
+import { deltaPost } from "~/helpers/api_helpers";
 import store from "../store";
-// import { withRetryHandling } from "../utils/retry-handling";
-
-const { get, post } = createApiClient(
-  "https://dev.gateway.matta.trade/flux/v1/"
-);
 
 const config = {
   headers: { Authorization: `Bearer ${store.getters.accessToken}` },
@@ -13,14 +8,14 @@ const config = {
 //Orders
 
 export const calculateCost = (data) => {
-  return post(`${urls.CALCULATOR_COST}`, data, {
+  return deltaPost(`${urls.CALCULATOR_COST}`, data, {
     ...config,
     withCredentials: false,
   });
 };
 
 export const getEnquiry = (data) => {
-  return post(`${urls.GET_ENQUIRY}`, data, {
+  return deltaPost(`${urls.GET_ENQUIRY}`, data, {
     ...config,
     withCredentials: false,
   });
