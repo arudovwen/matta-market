2e0b3b2aa2289a4f2f0c8d2a38f371c1236610f1
Success Ahon <40746773+arudovwen@users.noreply.github.com>
2026-05-28 20:59:57 +0100

    feat: add dynamic cart redirect page with authentication middleware

--- pages/cart/[id].vue
+++ pages/cart/[id].vue
@@ -1,13 +1,14 @@
 <template>
-    Carte
-    </template>
+  <div class="flex items-center justify-center h-screen w-screen">
+    <AppLoaderV2 />
+  </div>
+</template>
 <script setup>
-
 definePageMeta({
   middleware: "auth",
-})
+});
 
 onBeforeMount(() => {
-  navigateTo('/cart')
-})
+  navigateTo("/cart");
+});
 </script>
