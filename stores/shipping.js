ced4b93c4aefe272490971ae5b5b5003b5d44a8f
Success Ahon <40746773+arudovwen@users.noreply.github.com>
2026-06-01 09:55:53 +0100

    Update shipping.js

--- stores/shipping.js
+++ stores/shipping.js
@@ -9,7 +9,7 @@ export const useShippingStore = defineStore("shipping", () => {
   const defaultAddress = computed(() =>
     addresses.value.length
       ? addresses.value.find((i) => i.isDefault) || addresses.value[0]
-      : null
+      : null,
   );
 
   function setAddresses(data) {
@@ -30,11 +30,13 @@ export const useShippingStore = defineStore("shipping", () => {
     loading.value = true;
     getalladdress()
       .then(async (res) => {
-        if (res.data.data.length) {
+        if (res.status === 200) {
           const addresses = res.data.data;
-          if (addresses.length === 1 && !addresses[0].isDefault)
+          if (addresses.length === 1 && !addresses[0].isDefault) {
             await setdefaultaddress(addresses[0].id);
-          addresses[0].isDefault = true;
+            addresses[0].isDefault = true;
+          }
+
           setAddresses(addresses);
           const tempAddress = sortAddresses(addresses);
           setAddresses(tempAddress);
